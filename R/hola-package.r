2a3a3a5a53390a7b7f6c66c74dc8a5a10aefd99f
wrathematics <wrathematics@gmail.com>
2020-05-15 21:37:46 -0400

    first

---
+++ R/hola-package.r
@@ -0,0 +1,9 @@
+#' hola
+#' 
+#' An interface to the ADIOS2 library.
+#' 
+#' @name hola-package
+#' @docType package
+#' @author Drew Schmidt \email{wrathematics AT gmail.com}
+#' @keywords Package
+NULL
