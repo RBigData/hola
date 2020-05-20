f486be9bc23bd93d711c58cb9c633a608263685f
wrathematics <wrathematics@gmail.com>
2020-05-20 15:37:33 -0400

    native routines

---
+++ src/hola_native.c
@@ -0,0 +1,23 @@
+#include <R.h>
+#include <Rinternals.h>
+#include <R_ext/Rdynload.h>
+#include <stdlib.h>
+
+extern SEXP hola_available_variables(SEXP);
+extern SEXP hola_close(SEXP);
+extern SEXP hola_open(SEXP, SEXP);
+extern SEXP hola_read(SEXP, SEXP, SEXP);
+
+static const R_CallMethodDef CallEntries[] = {
+  {"hola_available_variables",  (DL_FUNC) &hola_available_variables,  1},
+  {"hola_close",                (DL_FUNC) &hola_close,                1},
+  {"hola_open",                 (DL_FUNC) &hola_open,                 2},
+  {"hola_read",                 (DL_FUNC) &hola_read,                 3},
+  {NULL, NULL, 0}
+};
+
+void R_init_hola(DllInfo *dll)
+{
+  R_registerRoutines(dll, NULL, CallEntries, NULL, NULL);
+  R_useDynamicSymbols(dll, FALSE);
+}
