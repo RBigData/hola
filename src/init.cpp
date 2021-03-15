5ec2f72d8c6b08dcaef8f861a88732e052820e57
wrathematics <wrathematics@gmail.com>
2021-03-15 12:23:14 -0400

    minor

--- src/init.cpp
+++ src/init.cpp
@@ -13,20 +13,18 @@ extern "C" SEXP hola_init(SEXP config, SEXP comm_ptr)
   if (config == R_NilValue)
     config_string = "";
   else
-    config_string = CHARPT(config, 0);;
+    config_string = CHARPT(config, 0);
   
   adios2::ADIOS *ad;
+  try {
 #ifdef MPI_VERSION
-  MPI_Comm comm = get_mpi_comm_from_Robj(comm_ptr);
-  try {
+    MPI_Comm comm = get_mpi_comm_from_Robj(comm_ptr);
     ad = new adios2::ADIOS(config_string, comm, "R", false);
-  } catch(const std::exception& e) { error(e.what()); }
 #else
-  (void)comm_ptr;
-  try {
+    (void)comm_ptr;
     ad = new adios2::ADIOS(config_string, "R", false);
-  } catch(const std::exception& e) { error(e.what()); }
 #endif
+  } catch(const std::exception& e) { error(e.what()); }
   
   newRptr(ad, ad_Robj, adios_object_finalizer<adios2::ADIOS>);
   
