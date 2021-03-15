86f0cb8add823294511f291bdab0b8799ab52dd7
wrathematics <wrathematics@gmail.com>
2021-03-15 11:42:32 -0400

    use explicit language constructor

--- src/init.cpp
+++ src/init.cpp
@@ -9,29 +9,22 @@ extern "C" SEXP hola_init(SEXP config, SEXP comm_ptr)
 {
   SEXP ad_Robj;
   
-  adios2::ADIOS *ad;
+  std::string config_string;
+  if (config == R_NilValue)
+    config_string = "";
+  else
+    config_string = CHARPT(config, 0);;
   
+  adios2::ADIOS *ad;
 #ifdef MPI_VERSION
   MPI_Comm comm = get_mpi_comm_from_Robj(comm_ptr);
   try {
-    if (config == R_NilValue)
-      ad = new adios2::ADIOS(comm);
-    else
-    {
-      std::string config_string = CHARPT(config, 0);
-      ad = new adios2::ADIOS(config_string, comm);
-    }
+    ad = new adios2::ADIOS(config_string, comm, "R", false);
   } catch(const std::exception& e) { error(e.what()); }
 #else
   (void)comm_ptr;
   try {
-    if (config == R_NilValue)
-      ad = new adios2::ADIOS;
-    else
-    {
-      std::string config_string = CHARPT(config, 0);
-      ad = new adios2::ADIOS(config_string);
-    }
+    ad = new adios2::ADIOS(config_string, "R", false);
   } catch(const std::exception& e) { error(e.what()); }
 #endif
   
