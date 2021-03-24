dd1210c597767c069954d5d98226521d3d4bccb1
wrathematics <wrathematics@gmail.com>
2021-03-24 11:43:51 -0400

    re-org

--- R/step.r
+++ R/step.r
@@ -1,3 +1,19 @@
+#' @useDynLib hola hola_begin_step
+adios_begin_step = function(af, timeout=10)
+{
+  .Call(hola_begin_step, af[[2]], as.double(timeout))
+}
+
+
+
+#' @useDynLib hola hola_advance
+adios_advance = function(af, step_begun, timeout=10)
+{
+  .Call(hola_advance, af[[2]], step_begun, as.double(timeout))
+}
+
+
+
 #' @useDynLib hola hola_step
 adios_step = function(af)
 {
