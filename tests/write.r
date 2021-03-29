4d540a1bf70b41a864fb531be84d891890182950
wrathematics <wrathematics@gmail.com>
2021-03-29 14:52:10 -0400

    add write test

--- tests/write.r
+++ tests/write.r
@@ -1,7 +1,7 @@
 suppressMessages(library(hola))
 
 f = tempdir()
-var = "/mydata"
+var = "/x"
 
 # --------------------------------------------------------------
 w = adios(f, mode="write")
@@ -10,23 +10,29 @@ x1w = matrix(1:8, 4)
 w$write(var, x1w)
 w$advance()
 
-x2w = matrix(8:1, 4)
+x2w = matrix(1L, 1, 1)
 w$write(var, x2w)
+w$advance()
+
+x3w = matrix(1:15, 3)
+w$write(var, x3w)
 w$advance()
 
 w$close()
 
 # --------------------------------------------------------------
 r = adios(f, mode="read")
+
 x1r = r$read(var)
-
 r$advance()
+
 x2r = r$read(var)
+r$advance()
 
+x3r = r$read(var)
 r$close()
-
-unlink(f, recursive=TRUE)
 
 # --------------------------------------------------------------
 stopifnot(all.equal(x1w, x1r))
 stopifnot(all.equal(x2w, x2r))
+stopifnot(all.equal(x3w, x3r))
