b7911ad60c315687569569f16621a67192d8f7ff
wrathematics <wrathematics@gmail.com>
2021-04-12 12:20:41 -0400

    change write object check

--- R/write.r
+++ R/write.r
@@ -1,8 +1,8 @@
 #' @useDynLib hola hola_write
 adios_write = function(af, variable, x)
 {
-  if (!is.vector(x) && !is.matrix(x))
-    stop("object 'x' must be a vector or matrix")
+  if (!is.atomic(x) && !is.vector(x) && !is.matrix(x) && !is.array(x)))
+    stop("object 'x' must be a vector, matrix, or n-dimensional array")
   
   .Call(hola_write, variable, af[[1]], af[[2]], x)
 }
