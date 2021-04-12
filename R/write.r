e59afd052f9fcd51c579b08bd3c072badf71904f
wrathematics <wrathematics@gmail.com>
2021-04-12 12:21:24 -0400

    parens

--- R/write.r
+++ R/write.r
@@ -1,7 +1,7 @@
 #' @useDynLib hola hola_write
 adios_write = function(af, variable, x)
 {
-  if (!is.atomic(x) && !is.vector(x) && !is.matrix(x) && !is.array(x)))
+  if (!is.atomic(x) && !is.vector(x) && !is.matrix(x) && !is.array(x))
     stop("object 'x' must be a vector, matrix, or n-dimensional array")
   
   .Call(hola_write, variable, af[[1]], af[[2]], x)
